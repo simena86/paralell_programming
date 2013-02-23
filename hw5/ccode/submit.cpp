f6d2a0fcd31a441c64e4e72ef7f7b0b27c795b26
Simen Andresen <simena86@gmail.com>
2013-02-22 16:24:14 -0800

    changd from modulo to if in neighbour count

--- hw5/ccode/submit.cpp
+++ hw5/ccode/submit.cpp
@@ -81,6 +81,44 @@ void print_matrix(int * a, int n){
 }
 
 
+int count_nbr(int *a, int n, int i, int j, int iters){
+	int nbr=0;
+	int i_south, i_north,j_west,j_east;
+	if(i==0){
+		i_north=n-1;
+		i_south=1;
+	}else if(i==n-1){
+		i_south=0;
+		i_north=n-2;
+	}else{
+		i_south=i+1;
+		i_north=i-1;
+	}if(j==0){
+		j_west=n-1;
+		j_east=1;
+	}else if(j==n-1){
+		j_east=0;
+		j_west=n-2;
+	}else{
+		j_east=j+1;
+		j_west=j-1;
+	}
+	nbr+=a[i*n + j_east];
+	nbr+=a[i*n + j_west];
+	nbr+=a[i_south*n + j];
+	nbr+=a[i_north*n + j ];
+	nbr+=a[i_north*n + j_west];
+	nbr+=a[i_north*n + j_east];
+	nbr+=a[i_south*n + j_west];
+	nbr+=a[i_south*n + j_east];
+	if(i==3 && j==2 && 1==2 ){
+		printf("%d %d %d %d %d %d %d %d \n\n",i*n+j_east, i*n + j_west, i_south*n+j, i_north*n+j,i_north*n+j_west, i_north*n+j_east, i_south*n+j_west, i_south*n+j_east );	
+		printf(" nbr: %d\n",nbr);
+	}
+	return nbr;
+}
+
+
 
 int mod (int a, int b){
    if(b < 0) //you can check for b == 0 separately and do what you want
@@ -105,7 +143,9 @@ void life(int *a, unsigned int n, unsigned int iter){
 		
 		for(int i=0;i<n;i++){	
 			for(int j=0;j<n;j++){
-				nbr=0;
+	//			print_matrix(a,n);
+				nbr=count_nbr(a_temp,n,i,j,iters);		
+				/*		nbr=0;
 				nbr+=a_temp[n*i+mod((j-1) , n)];  				//west
 				nbr+=a_temp[n*i+mod((j+1) , n)];  				//east
 				nbr+=a_temp[n*mod((i+1) , n) +j ]; 			//south
@@ -113,7 +153,7 @@ void life(int *a, unsigned int n, unsigned int iter){
 				nbr+=a_temp[n*mod((i+1) , n) +mod((j+1) , n)];	//south east
 				nbr+=a_temp[n*mod((i+1) , n) +mod((j-1) , n)];	//south west 
 				nbr+=a_temp[n*mod((i-1) , n) +mod((j+1) , n)];	//north east
-				nbr+=a_temp[n*mod((i-1) , n) +mod((j-1) , n)];	//north west
+				nbr+=a_temp[n*mod((i-1) , n) +mod((j-1) , n)];	//north west */
 				if(((a_temp[i*n+j]==1) && (nbr==2)) || (nbr==3)){
 					a[i*n+j]=1;
 				}else{
