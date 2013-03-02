bbef3dfb6b87cccc1dcd380331c9d4820848e098
Simen Andresen <simena86@gmail.com>
2013-03-02 00:02:18 -0800

    asd

--- final_project/ccode_simen/compute3LinkFreeWorkspace.c
+++ final_project/ccode_simen/compute3LinkFreeWorkspace.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "collision_detection.h"
 
 void rotateVector(double linkAngle, double* x, double* y){
 	double x_temp=*x;
 	double y_temp=*y;
-	*x=x*cos(linkAngle) - y*sin(linkAngle) 
+	*x=x_temp*cos(linkAngle) - y_temp*sin(linkAngle); 
 	*y=x_temp*sin(linkAngle) + y_temp*cos(linkAngle);
 }
 
 void displaceLinkPoly(double linkAngle,struct polygon* displacedLink, struct point* displacedLinkEnd, struct point linkBase,
-					struct polygon linkReference, struct linkEndPointReference ){
+					struct polygon linkReference, struct point linkEndPointReference ){
 	int i;
 	double xtemp,ytemp;
 	// translate and rotate all vertices of polygon	
-	for(i=0;i<linkReferece.numberOfVertices;i++){
-		xtemp=linkReference.x[i];
-		ytemp=linkReference.y[i];
+	for(i=0;i<linkReference.numberOfVertices;i++){
+		xtemp=linkReference.x_list[i];
+		ytemp=linkReference.y_list[i];
 		rotateVector(linkAngle,&xtemp,&ytemp );
-		displacedLink->x[i]=xtemp + linkBase.x;
-		displacedLink->y[i]=ytemp + linkBase.y;
+		displacedLink->x_list[i]=xtemp + linkBase.x;
+		displacedLink->y_list[i]=ytemp + linkBase.y;
 	}
 	// rotate and displace linkEnd
-	tempx=linkEndPointReference.x;
-	tempy=linkEndPointReference.y;
-	rotateVector(
-
-
+	xtemp=linkEndPointReference.x;
+	ytemp=linkEndPointReference.y;
+	rotateVector(linkAngle,&xtemp,&ytemp);
+	displacedLinkEnd->x=xtemp + linkBase.x;
+	displacedLinkEnd->y=ytemp + linkBase.y;
 }
+
+initTempPolys(struct polygon link1Poly, struct polygon link2Poly, struct polygon link3Poly,
+			struct polygon* displacedLink1 , struct polygon* displacedLink2, struct polygon* displacedLink3  ){
+	displacedLink1->numberOfVertices=link1Poly.numberOfVertices;
+	displacedLink1->x_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+	displacedLink1->y_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+	displacedLink2->numberOfVertices=link1Poly.numberOfVertices;
+	displacedLink2->x_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+	displacedLink2->y_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+	displacedLink3->numberOfVertices=link1Poly.numberOfVertices;
+	displacedLink3->x_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+	displacedLink3->y_list=(double*)malloc(link1Poly.numberOfVertices*sizeof(double));
+}
+
 
 void compute3LinkFreeWorkspace(unsigned int sample_list_length,double **sample_list,double **free_workspace,
-								double link1BaseRef,double link2BaseRef,double link2BaseRef,
+								struct point link1BaseRef,struct point link2BaseRef,struct point link3BaseRef,
 								struct polygon link1Poly, struct polygon link2Poly, struct polygon link3Poly){
+	int i,j,k;
+	struct polygon displacedLink1, displacedLink2, displacedLink3;
+	initTempPolys(link1Poly,link2Poly,link2Poly, &displacedLink1 , &displacedLink2 , &displacedLink3  );;
+	for(i=0;i<sample_list_length;i++){
+		for(j=0;j<sample_list_length;j++){
+			for(k=0;k<sample_list_length;k++){
+					
+			}
+		}
+	}
+		
+
 }
 
-		
+
+
+
+int main(){
+	return 0;
+
+
+}
