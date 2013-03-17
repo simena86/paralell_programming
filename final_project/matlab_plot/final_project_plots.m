3f2413bbd45f441d0fe31f1f5af720b664653f24
sverrkva <sverre.kvamme@gmail.com>
2013-03-16 23:21:54 -0700

    more plots added

--- final_project/matlab_plot/final_project_plots.m
+++ final_project/matlab_plot/final_project_plots.m
@@ -63,6 +63,47 @@ title('Paralell efficiency vs p, n=50');
 
 
 %%
+% Paralell efficiency  vs n
+
+
+% n = [10,15,20,25,30,35,40,45,50,55];
+n = [10,15,20,25,30,35,40,45];
+p = 8;
+
+
+%p=8
+%run_times_n_8 =  [ 0.00093,0.00825,0.04948,0.18875,0.52159,1.39290,3.11647,7.13247,15.52397,28.75429];
+run_times_n_8 =  [ 0.00093,0.00825,0.04948,0.18875,0.52159,1.39290,3.11647,7.13247];
+
+h(4)=figure(4);
+hold on; grid on;
+plot(n,run_times_n_8, '-ob', 'MarkerSize',20);
+xlabel('n');
+ylabel('Paralell efficiency');
+title('Paralell efficiency vs p, n=50');
+%%
+%
+
+% n = [10,15,20,25,30,35,40,45,50,55];
+n = [10,15,20,25,30,35,40,45];
+p = 8;
+
+%p=1
+run_times_n_1 = [0.00692,0.07450,0.42361,1.60599,4.71198,11.92388,26.56231,54.05625];
+%p=8
+%run_times_n_8 =  [ 0.00093,0.00825,0.04948,0.18875,0.52159,1.39290,3.11647,7.13247,15.52397,28.75429];
+run_times_n_8 = [0.00093,0.00825,0.04948,0.18875,0.52159,1.39290,3.11647,7.13247];
+pe = run_times_n_1./(p.*run_times_n_8);
+
+
+h(5)=figure(5);
+hold on; grid on;
+plot(n,pe, '-ob', 'MarkerSize',20);
+xlabel('n');
+ylabel('Paralell efficiency');
+title('Paralell efficiency vs n, p=8');
+
+%%
 for i=1:length(h)
     print(h(i), '-depsc', sprintf('plot%i.eps',i));
 end
